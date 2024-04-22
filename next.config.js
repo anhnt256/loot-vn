181ded5bfea1768c71034d7852b010db3342f70d
anhnt256 <anhnt256@gmail.com>
2024-04-23 03:09:51 +0700

    add timestamp

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const Timestamp = new Date().getTime();
+
 const nextConfig = {
   compress: false,
   reactStrictMode: true,
@@ -13,6 +16,32 @@ const nextConfig = {
       ],
     },
   ],
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      // Modifying the output filename for client-side bundles
+      config.output.filename = `static/chunks/[name].${Timestamp}.js`;
+      config.output.chunkFilename = `static/chunks/[name].${Timestamp}.js`;
+    }
+
+    // For CSS files, if you're using CSS modules
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: "css-loader",
+          options: {
+            modules: {
+              getLocalIdent: (context, localIdentName, localName) => {
+                return `${localName}_${Timestamp}`;
+              },
+            },
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
