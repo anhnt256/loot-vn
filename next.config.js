e7720568260a96d37f7475a251cda88880cb5717
anhnt256 <anhnt256@gmail.com>
2024-04-23 03:13:30 +0700

    remove css

--- next.config.js
+++ next.config.js
@@ -22,24 +22,6 @@ const nextConfig = {
       config.output.filename = `static/chunks/[name].${Timestamp}.js`;
       config.output.chunkFilename = `static/chunks/[name].${Timestamp}.js`;
     }
-
-    // For CSS files, if you're using CSS modules
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        {
-          loader: "css-loader",
-          options: {
-            modules: {
-              getLocalIdent: (context, localIdentName, localName) => {
-                return `${localName}_${Timestamp}`;
-              },
-            },
-          },
-        },
-      ],
-    });
-
     return config;
   },
 };
