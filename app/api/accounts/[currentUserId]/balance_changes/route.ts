8c0b5729644d5b3ac660e7fc304e17c7de644201
anhnt256 <anhnt256@gmail.com>
2025-01-06 18:26:36 +0700

    fix datetime

--- app/api/accounts/[currentUserId]/balance_changes/route.ts
+++ app/api/accounts/[currentUserId]/balance_changes/route.ts
@@ -12,20 +12,20 @@ export async function GET(
 
   const { currentUserId } = params;
 
-  // const startDate = dayjs()
-  //   .tz("Asia/Ho_Chi_Minh")
-  //   .startOf("day")
-  //   .format("YYYY-MM-DDTHH:mm:ss.SSSZ");
-  // const endDate = dayjs()
-  //   .tz("Asia/Ho_Chi_Minh")
-  //   .endOf("day")
-  //   .format("YYYY-MM-DDTHH:mm:ss.SSSZ");
+  const startDate = dayjs()
+    .tz("Asia/Ho_Chi_Minh")
+    .startOf("day")
+    .format("YYYY-MM-DDTHH:mm:ss.SSSZ");
+  const endDate = dayjs()
+    .tz("Asia/Ho_Chi_Minh")
+    .endOf("day")
+    .format("YYYY-MM-DDTHH:mm:ss.SSSZ");
 
-  // console.log("startDate", startDate);
-  // console.log("endDate", endDate);
+  console.log("startDate", startDate);
+  console.log("endDate", endDate);
 
-  const startDate = "2025-01-04T00:00:00.000+07:00";
-  const endDate = "2025-01-04T23:59:59.999+07:00";
+  // const startDate = "2025-01-04T00:00:00.000+07:00";
+  // const endDate = "2025-01-04T23:59:59.999+07:00";
 
   const url = `/accounts/${currentUserId}/balance_changes/?from_date=${encodeURIComponent(
     startDate,
