69c6a02bdedd688a809386efd6a11010a710192c
anhnt <anhnt256@gmail.com>
2025-02-08 10:55:15 +0700

    fix prisma

--- prisma/generated/fnet-gv-client/edge.js
+++ prisma/generated/fnet-gv-client/edge.js
@@ -1020,7 +1020,8 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null
+    "rootEnvPath": null,
+    "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
   "clientVersion": "5.22.0",
