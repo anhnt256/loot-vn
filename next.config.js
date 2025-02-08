91563d8773409d04dae92d5c6b888d61580b8343
anhnt <anhnt256@gmail.com>
2025-02-08 13:38:03 +0700

    fix buid

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const Timestamp = new Date().getTime();
+const CopyPlugin = require("copy-webpack-plugin");
 
 const nextConfig = {
   compress: false,
@@ -25,7 +26,26 @@ const nextConfig = {
       // Modifying the output filename for client-side bundles
       config.output.filename = `static/chunks/[name].${Timestamp}.js`;
       config.output.chunkFilename = `static/chunks/[name].${Timestamp}.js`;
+    } else {
+      config.externals = [
+        ...config.externals,
+        require("webpack-node-externals")(),
+      ];
     }
+    config.plugins.push(
+      new CopyPlugin({
+        patterns: [
+          {
+            from: "./node_modules/.prisma/client/schema.prisma",
+            to: "./prisma/generated/prisma-client",
+          },
+          {
+            from: "./node_modules/.prisma/client/schema.prisma",
+            to: "./prisma/generated/fnet-gv-client",
+          },
+        ],
+      }),
+    );
     return config;
   },
   experimental: {
@@ -34,6 +54,7 @@ const nextConfig = {
         "node_modules/.prisma/client/libquery_engine-rhel-openssl-1.0.x.so.node",
       ],
     },
+    serverActions: true,
   },
 };
 
