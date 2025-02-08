4edeb6049825a4ad680bccd54e076579036de4ae
anhnt <anhnt256@gmail.com>
2025-02-08 11:38:34 +0700

    fix prisma

--- app/(dashboard)/dashboard/_component/CheckInCalendar/CheckInCalendar.tsx
+++ app/(dashboard)/dashboard/_component/CheckInCalendar/CheckInCalendar.tsx
@@ -5,12 +5,12 @@ import { CheckInDay } from "@/type/checkin";
 import dayjs from "@/lib/dayjs";
 import isEmpty from "lodash/isEmpty";
 import { useQuery } from "@tanstack/react-query";
-import { CheckInItem, UserStarHistory } from ".@/prisma/generated/prisma-client";
 import { fetcher } from "@/lib/fetcher";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Card } from "@/components/ui/card";
 import { useUserInfo } from "@/hooks/use-user-info";
 import Image from "next/image";
+import { UserStarHistory } from "@/prisma/generated/prisma-client";
 
 const CheckInCalendar = () => {
   const { userBalance, userCheckIn } = useUserInfo();
