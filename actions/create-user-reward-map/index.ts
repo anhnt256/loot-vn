0f5b1dc27c8e64979c8635364d3b6d9e239347d5
DESKTOP-L06S5PI\Anh Nguyen <anhnt256@gmail.com>
2025-04-18 23:41:54 +0700

    fix đổi thưởng

--- actions/create-user-reward-map/index.ts
+++ actions/create-user-reward-map/index.ts
@@ -5,6 +5,12 @@ import { createSafeAction } from "@/lib/create-safe-action";
 import { CreateUserRewardMap } from "./schema";
 import { InputType } from "./type";
 import dayjs from "dayjs";
+import { NextResponse } from "next/server";
+import { signJWT } from "@/lib/jwt";
+import { cookies } from "next/headers";
+
+const expirationDuration = 1;
+const expirationDate = dayjs().add(expirationDuration, "day").format();
 
 const handler = async (data: InputType): Promise<any> => {
   const {
@@ -48,10 +54,7 @@ const handler = async (data: InputType): Promise<any> => {
           },
           data: {
             isUsed: false,
-            updatedAt: dayjs()
-              .tz("Asia/Ho_Chi_Minh")
-              .add(7, "hours")
-              .toISOString(),
+            updatedAt: dayjs().add(7, "hours").toISOString(),
           },
         });
 
@@ -62,20 +65,21 @@ const handler = async (data: InputType): Promise<any> => {
           select: {
             UserId: true,
             RemainMoney: true,
-            Birthdate: true,
-            ExpiryDate: true,
           },
         });
 
         if (fnetUser) {
+          const today = new Date();
+          const todayFormatted = today.toISOString().split('T')[0] + 'T00:00:00.000Z';
+
           await fnetDB.usertb.update({
             where: {
               UserId: userId,
             },
             data: {
               RemainMoney: Number(fnetUser.RemainMoney) + Number(value),
-              Birthdate:  fnetUser.Birthdate || null,
-              ExpiryDate: fnetUser.ExpiryDate || null,
+              Birthdate: todayFormatted,
+              ExpiryDate: todayFormatted
             },
           });
         }
@@ -88,10 +92,7 @@ const handler = async (data: InputType): Promise<any> => {
             duration,
             isUsed,
             branch,
-            createdAt: dayjs()
-              .tz("Asia/Ho_Chi_Minh")
-              .add(7, "hours")
-              .toISOString(),
+            createdAt: dayjs().add(7, "hours").toISOString(),
           },
         });
 
@@ -102,10 +103,7 @@ const handler = async (data: InputType): Promise<any> => {
             },
             data: {
               stars: newStars,
-              updatedAt: dayjs()
-                .tz("Asia/Ho_Chi_Minh")
-                .add(7, "hours")
-                .toISOString(),
+              updatedAt: dayjs().add(7, "hours").toISOString(),
             },
           });
 
@@ -116,10 +114,7 @@ const handler = async (data: InputType): Promise<any> => {
               oldStars,
               newStars,
               targetId: rewardId,
-              createdAt: dayjs()
-                .tz("Asia/Ho_Chi_Minh")
-                .add(7, "hours")
-                .toISOString(),
+              createdAt: dayjs().add(7, "hours").toISOString(),
               branch,
             },
           });
@@ -140,3 +135,155 @@ export const createUserRewardMap = createSafeAction(
   CreateUserRewardMap,
   handler,
 );
+
+export async function POST(req: Request, res: Response): Promise<any> {
+  const cookieStore = await cookies();
+  const branchFromCookie = cookieStore.get("branch")?.value;
+
+  try {
+    const body = await req.text();
+
+    const { userName, machineName } = JSON.parse(body);
+
+    const fnetDB = await getFnetDB();
+    const fnetPrisma = await getFnetPrisma();
+
+    const query = fnetPrisma.sql`SELECT userId
+                                 FROM fnet.systemlogtb AS t1
+                                 WHERE t1.MachineName = ${machineName}
+                                 ORDER BY STR_TO_DATE(CONCAT(t1.EnterDate, ' ', t1.EnterTime), '%Y-%m-%d %H:%i:%s') DESC
+                                   LIMIT 1`;
+
+    const user: any = await fnetDB.$queryRaw<any>(query);
+
+    // const userId = user[0]?.userId ?? null;
+
+    const userId = 2969;
+
+    let userUpdated;
+
+    const currentUsers = await db.user.findMany({
+      where: {
+        userId: Number(userId),
+        branch: branchFromCookie,
+      },
+    });
+
+    const validUserNames = currentUsers
+      .map((user) => user.userName)
+      .filter(
+        (userName): userName is string =>
+          userName !== null && userName.trim() !== "",
+      );
+
+    if (validUserNames.length > 0) {
+      const usersByUsername = await db.user.findMany({
+        where: {
+          userName: { in: validUserNames },
+        },
+      });
+
+      const uniqueBranches = new Set(
+        usersByUsername.map((user) => user.branch),
+      );
+
+      if (uniqueBranches.size > 1) {
+        return NextResponse.json("Duplicate account", { status: 499 });
+      }
+
+      const thisUsers = [...currentUsers, ...usersByUsername];
+      const allUsers = [
+        ...new Map(thisUsers.map((user) => [user.id, user])).values(),
+      ];
+
+      if (allUsers.length > 1) {
+        allUsers.sort(
+          (a: { updatedAt: Date }, b: { updatedAt: Date }) =>
+            new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime(),
+        );
+
+        const latestUser = allUsers[0];
+        const totalStars = allUsers.reduce(
+          (sum: number, u: { stars: number }) => sum + u.stars,
+          0,
+        );
+        const totalMagicStones = allUsers.reduce(
+          (sum: number, u: { magicStone: number }) => sum + u.magicStone,
+          0,
+        );
+
+        userUpdated = await db.user.update({
+          where: { id: latestUser.id },
+          data: {
+            stars: totalStars,
+            magicStone: totalMagicStones,
+            userId: userId,
+          },
+        });
+
+        const deleteIds = allUsers
+          .slice(1)
+          .map((u: { id: number }) => u.id)
+          .filter((id: number) => id !== latestUser.id);
+
+        if (deleteIds.length > 0) {
+          await db.userMissionMap.deleteMany({
+            where: { userId: { in: deleteIds } },
+          });
+        }
+
+        await db.user.deleteMany({
+          where: { id: { in: deleteIds } },
+        });
+      } else {
+        userUpdated = await db.user.update({
+          where: { id: allUsers[0].id },
+          data: {
+            userId: userId,
+          },
+        });
+      }
+    } else {
+      if (branchFromCookie) {
+        userUpdated = await db.user.create({
+          data: {
+            userName: userName.trim(),
+            userId,
+            branch: branchFromCookie,
+            rankId: 1,
+            stars: 0,
+            magicStone: 0,
+            createdAt: dayjs().add(7, "hours").toISOString(),
+          },
+        });
+      }
+    }
+
+    if (userUpdated) {
+      const token = await signJWT({ userId: userUpdated?.userId });
+      const response = NextResponse.json(userUpdated);
+
+      response.cookies.set({
+        name: "token",
+        value: token,
+        maxAge: 86400,
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        path: "/",
+      });
+
+      return response;
+    }
+    return NextResponse.json("Login Fail", { status: 401 });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "Internal Error";
+    return new NextResponse(JSON.stringify({ error: errorMessage }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
